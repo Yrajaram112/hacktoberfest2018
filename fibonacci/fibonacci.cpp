d77f801f5b971ad58c0a169ece0b249c4dd0ee9d
Rajaram Yadav <yrajaram112@gmail.com>
2020-10-12 21:21:21 +0530

    Update fibonacci.cpp

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -15,3 +15,30 @@ int main ()
     cout<<fib(n);
     return 0;
 }
+
+
+//Fibonacci series using loop
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"No of elements in series"<<endl;
+   cin>>n;
+   int a=0;
+   int b=1;
+   int i=0;
+   while(i<n){
+       
+       if(i==n)
+       break;
+       cout<<a<<"  ";
+       i++;
+       if(i==n)
+       break;
+       cout<<b<<"  ";
+       i++;
+       a=a+b;
+       b=b+a;
+   
+   }
+}
